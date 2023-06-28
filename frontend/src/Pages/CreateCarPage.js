b9e70aab7e52dac096322e14064e9b67f6276b5f
fcoscar <oscarjr01.13@gmail.com>
2023-06-28 17:03:28 -0400

    create car page

---
+++ frontend/src/Pages/CreateCarPage.js
@@ -0,0 +1,11 @@
+import {useSelector} from "react-redux";
+
+function CreateCarPage(){
+    const auth = useSelector(state => state.auth)
+    const {userInfo} = auth
+    return(
+
+    )
+}
+
+export default CreateCarPage
